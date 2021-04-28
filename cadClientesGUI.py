f538643203284650f7d25b65f33ea3468d1563f3
Elvis sousa Brandão <lskbca_elvis19@hotmail.com>
2021-04-28 13:38:51 -0300

    Update cadClientesGUI.py
    Implementação de algumas melhorias, tal como limpar todos os campos após um cadastro, como também mostrar um popup quando realizar um cadastro.

--- cadClientesGUI.py
+++ cadClientesGUI.py
@@ -22,54 +22,55 @@ cursor.execute("""CREATE TABLE IF NOT EXISTS clientes (
 ##################################################
 
 # criando a janela (GUI)
-sg.theme('Light Gray1')   # Add a touch of color
-# Desingn of the window.
-layout = [  [sg.Text('Nome:', size=(6, 1)), sg.InputText(size=(30,1), key='-NOME-')],
-            [sg.Text('Idade:', size=(6, 1)), sg.InputText(size=(30,1), key='-IDADE-')],
-            [sg.Text('CPF:', size=(6, 1)), sg.InputText(size=(30,1), key='-CPF-')],
-            [sg.Text('e-mail:', size=(6, 1)), sg.InputText(size=(30,1), key='-EMAIL-')],
-            [sg.Text('Fone:', size=(6, 1)), sg.InputText(size=(30,1), key='-FONE-')],
-            [sg.Text('Cidade:', size=(6, 1)), sg.InputText(size=(30,1), key='-CIDADE-')],
-            [sg.Text('UF:', size=(6, 1)), sg.InputText(size=(30,1), key='-UF-')],
-            [ sg.Button('Cadastrar', button_color = 'black on orange', font = ['Comics', 12]), 
-            sg.Button('Sair', button_color = 'black on red', font = ['Comics', 12]),
-            sg.Button('Ver Registros', button_color = 'black on green', font = ['Comics', 12]) ],
-            [sg.Output(size=(36,8), key='-OUTPUT-')] ]
+sg.theme('Tan Blue')   # Definindo um tema para a janela
+# Desingn da janela.
+layout = [  [sg.Text('Nome:', size=(6, 1)), sg.InputText(size=(30,1), key='-NOME-')],
+            [sg.Text('Idade:', size=(6, 1)), sg.InputText(size=(30,1), key='-IDADE-')],
+            [sg.Text('CPF:', size=(6, 1)), sg.InputText(size=(30,1), key='-CPF-')],
+            [sg.Text('e-mail:', size=(6, 1)), sg.InputText(size=(30,1), key='-EMAIL-')],
+            [sg.Text('Fone:', size=(6, 1)), sg.InputText(size=(30,1), key='-FONE-')],
+            [sg.Text('Cidade:', size=(6, 1)), sg.InputText(size=(30,1), key='-CIDADE-')],
+            [sg.Text('UF:', size=(6, 1)), sg.InputText(size=(30,1), key='-UF-')],
+            [ sg.Button('Cadastrar', button_color = 'black on orange', font = ['Comics', 12]), 
+            sg.Button('Sair', button_color = 'black on red', font = ['Comics', 12]),
+            sg.Button('Ver Registros', button_color = 'black on green', font = ['Comics', 12]) ],
+            [sg.Output(size=(36,8), key='-OUTPUT-')] ]
 
-# Create the Window
+# Criando a janela
 window = sg.Window('Cadastro de Clientes', layout)
 
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
-    event, values = window.read()
-    # if user closes window or clicks exit
-    if event == sg.WIN_CLOSED or event == 'Sair':
-        break
-    if event == 'Cadastrar':
-        try:
-            # inserindo dados na tabela
-            cursor.execute("""INSERT INTO clientes (nome, idade, cpf, email, fone, cidade, uf)
-            VALUES (?,?,?,?,?,?,?)""", (values['-NOME-'], values['-IDADE-'], values['-CPF-'], 
-            values['-EMAIL-'], values['-FONE-'], values['-CIDADE-'], values['-UF-']))
-            conn.commit() #NUNCA ESQUECER ESSE COMANDO
-            print('Cliente cadastrado com sucesso!')
-            window['-NOME-'].VALUES = ''
-            window['-IDADE-'].VALUES = ''
-            window['-CPF-'].VALUES = ''
-            window['-EMAIL-'].VALUES = ''
-            window['-FONE-'].VALUES = ''
-            window['-CIDADE-'].VALUES = ''
-            window['-UF-'].VALUES = ''
-        except E:
-            print(E)
-    if event == 'Ver Registros':
-        # lendo os dados
-        print('\nOs dados salvos na base de dados são:')
+    event, values = window.read()
+    # if user closes window or clicks exit
+    if event == sg.WIN_CLOSED or event == 'Sair':
+        break
+    if event == 'Cadastrar':
+        try:
+            # inserindo dados na tabela
+            cursor.execute("""INSERT INTO clientes (nome, idade, cpf, email, fone, cidade, uf)
+            VALUES (?,?,?,?,?,?,?)""", (values['-NOME-'], values['-IDADE-'], values['-CPF-'], 
+            values['-EMAIL-'], values['-FONE-'], values['-CIDADE-'], values['-UF-']))
+            conn.commit() #NUNCA ESQUECER ESSE COMANDO
+            #print('Cliente cadastrado com sucesso!')
+            sg.popup('Cliente cadastrado com sucesso!')
+            window['-NOME-'].update('')
+            window['-IDADE-'].update('')
+            window['-CPF-'].update('')
+            window['-EMAIL-'].update('')
+            window['-FONE-'].update('')
+            window['-CIDADE-'].update('')
+            window['-UF-'].update('')
+        except E:
+            print(E)
+    if event == 'Ver Registros':
+        # lendo os dados
+        print('\nOs dados salvos na base de dados são:')
 
-        cursor.execute("""SELECT * FROM clientes;""")
+        cursor.execute("""SELECT * FROM clientes;""")
 
-        for linha in cursor.fetchall():
-            print(linha)        
+        for linha in cursor.fetchall():
+            print(linha)        
 
 conn.close() # desconectando do banco de dados...
 window.close()
